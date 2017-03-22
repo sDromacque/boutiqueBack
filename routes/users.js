ee6b9f28c21999122f4bb53dfb8c8e84510b95b7
seb <sdromacquedev@gmail.com>
2017-03-22 15:13:28 +0100

    Implemante get /user/:id

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', (req, res, next) => {
     res.json('ok');
 });
 
+router.get('/:id', (req, res) => {
+
+});
+
 module.exports = router;
--- test/controllers/users.js
+++ test/controllers/users.js
@@ -1,11 +1,23 @@
 const request = require('supertest');
 const express = require('express');
 const app = express('../../bin/www');
+const chai = require('chai');
+const assert = chai.assert;
 
 describe('GET /users', () => {
     it('respond with json', () => {
         request(app)
             .get('/users')
             .expect(200);
+    });
+
+    it('GET /:id', () => {
+        let user = new User({
+            name: 'Bob',
+            lastname: 'éponge'
+        });
+       request(app)
+           .get('/users/:id')
+           .expect(200);
     });
 });
