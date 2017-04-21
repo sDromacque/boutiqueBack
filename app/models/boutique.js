a2625234af82c4399cb7ca61e7dd3b2773abdd58
seb <seb>
2017-04-21 14:49:58 +0200

    Update model boutique

--- app/models/boutique.js
+++ app/models/boutique.js
@@ -1,33 +1,38 @@
 const mongoose = require( 'mongoose' );
-const Schema = mongoose.Schema;
 
-module.exports = (mongoose) => {
-  const BoutiqueSchema = new Schema({
-    name: {
+let BoutiqueSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  loc: {
+    type: {
       type: String,
-      required: true,
+      default: 'Point'
     },
-    email: {
-      type: String,
-      required: true
-    },
-    loc: {
-      type: mongoose.Schema.Types.Point,
-      required: true
-    },
-    created_at: {
-      type: Date,
-      default: Date.now
-    },
-    updated_at: {
-      type: Date,
-      default: Date.now
-    }
-  });
+    coordinates: []
+  },
+  // loc: {
+  //   type: {
+  //     type: String,
+  //     default:'Point'
+  //   },
+  //   coordinates: [Number]
+  // },
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now
+  }
+});
 
-  BoutiqueSchema.pre('save', function (next) {
-    this.updated_at = Date.now();
-    next();
-  });
-  const Boutique = mongoose.model('Boutique', BoutiqueSchema);
-};
+let Boutique = mongoose.model('Boutique', BoutiqueSchema);
+
+module.exports = Boutique;
