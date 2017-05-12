1e9edd4f9390a481ee453cd4d4d9d956dfc71a02
sdromacque <sebastien@citymagine.com>
2017-05-12 16:20:51 +0200

    Update model User

--- app/models/user.js
+++ app/models/user.js
@@ -2,11 +2,22 @@ let mongoose = require( 'mongoose' ),
   Schema = mongoose.Schema
 ;
 
+const validator = require('validator');
+
 let UserSchema = new Schema({
-  name: String,
+  name: {
+    type: String,
+    required: true
+  },
   username: String,
   lastname: String,
-  email: String
+  email: {
+    type: String,
+    required: true,
+    validate: email => {
+      return validator.isEmail(email);
+    }
+  }
 });
 
 let User = mongoose.model('User', UserSchema);
