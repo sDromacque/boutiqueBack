2a3d4857b0c3915b2e60e1208ce4f01db253bca9
sdromacque <sdromacquedev@gmail.com>
2017-07-20 14:00:25 +0200

    update model user

--- app/models/user.js
+++ app/models/user.js
@@ -1,61 +1,74 @@
-let mongoose = require( 'mongoose' ),
-  Schema = mongoose.Schema
+const mongoose = require( 'mongoose' ),
+      Schema = mongoose.Schema,
+      validator = require('validator'),
+      Promise = require('bluebird')
+      bcrypt = require("bcrypt")
 ;
-const bcrypt   = require('bcrypt');
 
-const validator = require('validator');
+// const bcrypt = Promise.promisifyAll(require("bcrypt"));
 
 let UserSchema = new Schema({
-  // firstName: {
-  //   type: String,
-  //   required: true
-  // },
-  // username: {
-  //   type: String,
-  //   required: true
-  // },
-  // lastName: {
-  //   type: String,
-  //   required: true
-  // },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  username: {
+    type: String,
+  },
   email: {
     type: String,
     required: true,
     validate: email => {
       return validator.isEmail(email);
     },
-    unique: true
+    // unique: true
   },
-  // address:{
-  //   zipCode: {
-  //     type: Number,
-  //     required: true
-  //   },
-  //   city: {
-  //     type: String,
-  //     required: true
-  //   },
-  //   country: {
-  //     type: String,
-  //     required: true
-  //   },
-  //   streetAddress: {
-  //     type: String,
-  //     required: true
-  //   }
-  // },
-
+  address:{
+    zipCode: {
+      type: Number,
+    },
+    city: {
+      type: String,
+    },
+    country: {
+      type: String,
+    },
+    streetAddress: {
+      type: String,
+    }
+  },
   password: {
       type: String,
       required: true
   },
   role: {
       type: String,
-      enum: ['reader', 'creator', 'editor'],
-      default: 'reader'
+      enum: ['customer', 'pro', 'admin'],
+      default: 'customer'
   }
 });
 
+UserSchema.pre('save', function(next){
+
+    let user = this;
+    /* if the user's password has not been modified, do not continue with
+    hashing their password....this is for updating a user. If a user does not
+    update their password, do not create a new password hash! */
+    if (!user.isModified('password')) return next();
+
+    // if the user has modified their password, let's hash it
+    bcrypt.hash(user.password, 10).then(function(hashedPassword) {
+        // then let's set their password to not be the plain text one anymore, but the newly hashed password
+        user.password = hashedPassword;
+        // then we save the user in the db!
+        next();
+    }, function(err){
+        // or we continue and pass in an error that has happened (which our express error handler will catch)
+        return next(err);
+    })
+});
 
 UserSchema.methods.comparePassword = function (candidatePassword, cb) {
   bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
